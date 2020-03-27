0729c2e2c0bf2f2966c85b2af02de6f30e95d843
Jess D'Souza <--unset>
2020-03-27 16:33:28 +0400

    minor update

--- js/main.js
+++ js/main.js
@@ -4,21 +4,26 @@ const body = document.querySelector("body");
 
 const nightmode_toggle = document.querySelector("#switch");
 
+// landing page time out when the page loads.
 window.onload = () => {
-	nightmode_toggle.checked = true;
+    window.setTimeout(() => {
+        landing_page.style.display = "none";
+        body.setAttribute("dark", "");
+        body.removeAttribute("landing");
+        main_page.style.display = "initial";
+        nightmode_toggle.checked = true;
+    }, 4000);
 };
 
+// toggle switch settings to switch from light mode to dark mode
 nightmode_toggle.addEventListener("change", (event) => {
-	if (nightmode_toggle.checked === true) {
-		body.setAttribute("dark", "");
-		body.removeAttribute("light");
-	} else {
-		body.setAttribute("light", "");
-		body.removeAttribute("dark");
-	}
+    if (nightmode_toggle.checked === true) {
+        body.setAttribute("dark", "");
+        body.removeAttribute("light");
+        body.removeAttribute("landing");
+    } else {
+        body.setAttribute("light", "");
+        body.removeAttribute("dark");
+        body.removeAttribute("landing");
+    }
 });
-
-// window.setTimeout(() => {
-// 	landing_page.style.display = "none";
-// 	main_page.style.display = "initial";
-// }, 4000);
